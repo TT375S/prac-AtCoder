768a080996a60a3422ac96f21c21a7a07fc65b93
TT375S <tomataka375@gmail.com>
2017-12-10 21:21:44 +0900

    Change DFS

--- CodeForeces/NEERC–NorthernEurasiaFinals2017/C_Connections.cpp
+++ CodeForeces/NEERC–NorthernEurasiaFinals2017/C_Connections.cpp
@@ -21,121 +21,139 @@
 using namespace std;
 
 #define MAX_V 100000
-#define INF INT_MAX/4
 #define EMPTY -1
 
-bool DEBUG = false;
-
-typedef pair<int, int> P;
+bool DEBUG =true;
 
 struct Edge {
-	int to, cap, cost, rev;
+	long long to;
+	long long revInd;
 	bool isUsed;
 };
-vector<Edge> G[MAX_V];
-vector<Edge> revG[MAX_V];
 
-struct Node {
-	int dist, prevV, prevE;
-};
-vector<Node> V;
+vector<long long> usedEdges;
 
-//DFSで有向スパニングツリーを作成。使用済み枝を記録
-void dfs(vector<Node> nodes, vector<Edge> edges[]) {
-	bool *isUsedNode;
-	isUsedNode = (bool *) malloc(nodes.size() * sizeof(bool));
-	memset(isUsedNode, false, nodes.size() * sizeof(bool));
-
-	isUsedNode[0] = true;
-
-	for (int i_v = 0; i_v < nodes.size(); i_v++) {
-		for (int i_e = 0; i_e < edges[i_v].size(); i_e++) {
-			Edge &e = edges[i_v][i_e];
-			//到達済みなら何もしない
-			if (isUsedNode[e.to] == true)
-				continue;
-			//ノードに到達したことにする
-			isUsedNode[e.to] = true;
-			e.isUsed = true;
+void recDFS(long long ind_node, vector<Edge> edges[], bool isUsedNode[]) {
+	//if(isUsedNode == true) return;
+	isUsedNode[ind_node] = true;
+	if(DEBUG) printf("   %d\n", ind_node+1);
+	for (long long i_e = 0; i_e < edges[ind_node].size(); i_e++) {
+		Edge &e = edges[ind_node][i_e];
+		//if (DEBUG)			printf("%d to %d, isUsed:%d\n", ind_node + 1, e.to + 1,	isUsedNode[e.to]);
+		if (isUsedNode[e.to] == false) {
+			edges[ind_node][i_e].isUsed = true;
+			recDFS(e.to, edges, isUsedNode);
 		}
 	}
-
-	free(isUsedNode);
 }
 
-//使用されなかった枝(弧)を削除
-void deleteUnusedArc(vector<Node> nodes, vector<Edge> edges[],
-		vector<Edge> revEdges[], int *lines) {
-	for (int i_v = 0; i_v < nodes.size(); i_v++) {
-		for (int i_e = 0; i_e < edges[i_v].size(); i_e++) {
-			Edge &e = edges[i_v][i_e];
-			Edge &re = revEdges[e.to][e.rev];
-			//未使用なら削除する。
-			if (e.isUsed == false && re.isUsed == false && *lines >= 1) {
-				(*lines)--;
-				printf("%d %d\n", i_v + 1, e.to + 1);
-				//2回表示してしまわないようにこうする
-				e.isUsed = true;
-				re.isUsed = true;
-			}
+//int vector_finder(std::vector<long long > vec, long long number) {
+//  auto itr = std::find(vec.begin(), vec.end(), number);
+//  size_t index = std::distance( vec.begin(), itr );
+//  if (index != vec.size()) { // 発見できたとき
+//    return 1;
+//  }
+//  else { // 発見できなかったとき
+//    return 0;
+//  }
+//}
+
+void showEdge(long long num_v, vector<Edge> *G) {
+	for (int i_v = 0; i_v < num_v; i_v++) {
+		for (int i_e = 0; i_e < G[i_v].size(); i_e++) {
+			Edge &e = G[i_v][i_e];
+			printf("%d to %d isUsed:%d\n", i_v + 1, e.to + 1, e.isUsed);
 		}
 	}
-
-	for (int i_v = 0; i_v < nodes.size(); i_v++) {
-		for (int i_e = 0; i_e < revEdges[i_v].size(); i_e++) {
-			Edge &re = revEdges[i_v][i_e];
-			Edge &e = edges[re.to][re.rev];
-			//未使用なら削除する。
-			if (e.isUsed == false && re.isUsed == false && *lines >= 1) {
-				(*lines)--;
-				printf("%d %d\n", re.to + 1, i_v + 1);
-			}
-		}
-	}
-
 }
 
-void add_edge(int from, int to, int cap, int cost) {
-	int ind_rev_f = revG[to].size();
-	int ind_rev_b = G[from].size();
-	//コストとキャパは使わないが削除が面倒なので残している
-	G[from].push_back((Edge ) { to, cap, cost, ind_rev_f, false });
-	revG[to].push_back((Edge ) { from, 0, -1 * cost, ind_rev_b, false });
+void revShowEdge(long long num_v, vector<Edge> *G) {
+	for (int i_v = 0; i_v < num_v; i_v++) {
+		for (int i_e = 0; i_e < G[i_v].size(); i_e++) {
+			Edge &e = G[i_v][i_e];
+			printf("%d to %d isUsed:%d\n", e.to + 1, i_v + 1, e.isUsed);
+		}
+	}
 }
 
 int main() {
-	int times = 0;
+	long long times = 0;
 	cin >> times;
-	for (int i_times = 0; i_times < times; i_times++) {
-		int n, m;
+	for (long long i_times = 0; i_times < times; i_times++) {
+		long long n, m;
 		cin >> n >> m;
+		vector<Edge> G[MAX_V];
+		vector<Edge> revG[MAX_V];
 
-		if (DEBUG)
-			printf("INPUT: n:%d m:%d\n", n, m);
-
-		V.resize(n);
-		for (int i_road = 0; i_road < m; i_road++) {
-			int x, y;
+		for (long long i_road = 0; i_road < m; i_road++) {
+			long long x, y;
 			cin >> x >> y;
 			//インデックスが0オリジン想定なのに入力は1オリジンだったりするのは気をつけろ
 			x--;
 			y--;
-			if (DEBUG)
-				printf("INPUT:i:%d x:%d y:%d\n", i_road, x, y);
-			//コストとキャパは使わないが削除が面倒なので残している
-			add_edge(x, y, 1, 0);
+			long long ind_G_rev = revG[y].size();
+			long long ind_revG_rev = G[x].size();
+			G[x].push_back((Edge ) { y, ind_G_rev, false });
+			revG[y].push_back((Edge ) { x, ind_revG_rev, false });
+			if (DEBUG){
+				printf("input: %d to %d\n", x, y);
+				printf("size: %d , %d\n", G[x].size(), revG[y].size());
+				Edge &e = G[x][G[x].size()-1];
+				Edge &re = revG[e.to][e.revInd];
+				printf("%d to %d AND %d to %d\n", x, e.to, e.to, re.to);
+				showEdge(n, G);
+				printf("rev\n");
+				showEdge(n, revG);
+			}
 		}
 
-		//有向のスパニングツリー作成。使用した弧を記録する
-		dfs(V, G);
-		dfs(V, revG);
+		bool *isUsedNode;
+		isUsedNode = (bool *) malloc(n * sizeof(bool));
 
-		int lines = m - 2 * n;
+		memset(isUsedNode, false, n * sizeof(bool));
+		//有向のスパニングツリー作成。0(市1)を根とする。使用した弧を記録する
+		recDFS(0, G, isUsedNode);
 		if (DEBUG)
-			printf("lines:%d\n", lines);
+			printf("rev\n");
+		memset(isUsedNode, false, n * sizeof(bool));
+		recDFS(0, revG, isUsedNode);
+
+		free(isUsedNode);
+
+		//long long lines = 1000;
+		long long lines = m - 2 * n;
+		//long long lines = INF;
+		if (DEBUG)
+			showEdge(n, G);
+		if (DEBUG)
+			printf("rev\n");
+		if (DEBUG)
+			revShowEdge(n, revG);
 
 		//未使用の弧を削除する
-		deleteUnusedArc(V, G, revG, &lines);
+		//deleteUnusedArc(V, G, revG, &lines);
+		for (int i_v = 0; i_v < n; i_v++) {
+			for (int i_e = 0; i_e < G[i_v].size(); i_e++) {
+				Edge &e = G[i_v][i_e];
+				Edge &re = revG[e.to][e.revInd];
+
+				if (DEBUG){
+					printf(" %d to %d\n", i_v+1, e.to+1);
+					printf(" %d to %d   AND  %d to %d\n", i_v+1, e.to+1, e.to+1, re.to+1);
+					printf("n %d to %d isUsed:%d\n", i_v + 1, i_e + 1,
+							G[i_v][i_e].isUsed);
+					printf("r %d to %d isUsed:%d\n", e.to + 1, i_e + 1,
+							revG[e.to][i_e].isUsed);
+				}
+
+				if (e.isUsed == false
+						&& re.isUsed == false) {
+					if(lines <= 0)break;
+					lines--;
+					printf("%d %lld\n", i_v + 1, e.to + 1);
+				}
+			}
+		}
 
 		return 0;
 	}
