1aca19189acc9e4c67b6bdf4a79938f905ae1300
TT375S <tomataka375@gmail.com>
2017-12-10 16:49:48 +0900

    Fix NERRC17-C.Connection. インデックスを0オリジンに合わせた 。あと削除が重複して行われていたのを修正

--- CodeForeces/NEERC–NorthernEurasiaFinals2017/C_Connections.cpp
+++ CodeForeces/NEERC–NorthernEurasiaFinals2017/C_Connections.cpp
@@ -64,20 +64,35 @@ void dfs(vector<Node> nodes, vector<Edge> edges[]) {
 }
 
 //使用されなかった枝(弧)を削除
-void deleteUnusedArc(vector<Node> nodes, vector<Edge> edges[], bool isRev, int *lines) {
+void deleteUnusedArc(vector<Node> nodes, vector<Edge> edges[],
+		vector<Edge> revEdges[], int *lines) {
 	for (int i_v = 0; i_v < nodes.size(); i_v++) {
 		for (int i_e = 0; i_e < edges[i_v].size(); i_e++) {
 			Edge &e = edges[i_v][i_e];
+			Edge &re = revEdges[e.to][e.rev];
 			//未使用なら削除する。
-			if (e.isUsed == false && *lines >= 1) {
-				(*lines) --;
-				if (!isRev)
-					printf("%d %d\n", i_v, e.to);
-				else
-					printf("%d %d\n", e.to, i_v);
+			if (e.isUsed == false && re.isUsed == false && *lines >= 1) {
+				(*lines)--;
+				printf("%d %d\n", i_v + 1, e.to + 1);
+				//2回表示してしまわないようにこうする
+				e.isUsed = true;
+				re.isUsed = true;
 			}
 		}
 	}
+
+	for (int i_v = 0; i_v < nodes.size(); i_v++) {
+		for (int i_e = 0; i_e < revEdges[i_v].size(); i_e++) {
+			Edge &re = revEdges[i_v][i_e];
+			Edge &e = edges[re.to][re.rev];
+			//未使用なら削除する。
+			if (e.isUsed == false && re.isUsed == false && *lines >= 1) {
+				(*lines)--;
+				printf("%d %d\n", re.to + 1, i_v + 1);
+			}
+		}
+	}
+
 }
 
 void add_edge(int from, int to, int cap, int cost) {
@@ -95,13 +110,18 @@ int main() {
 		int n, m;
 		cin >> n >> m;
 
-		if(DEBUG) printf("INPUT: n:%d m:%d\n",n, m);
+		if (DEBUG)
+			printf("INPUT: n:%d m:%d\n", n, m);
 
 		V.resize(n);
 		for (int i_road = 0; i_road < m; i_road++) {
 			int x, y;
 			cin >> x >> y;
-			if(DEBUG) printf("INPUT:i:%d x:%d y:%d\n", i_road, x, y);
+			//インデックスが0オリジン想定なのに入力は1オリジンだったりするのは気をつけろ
+			x--;
+			y--;
+			if (DEBUG)
+				printf("INPUT:i:%d x:%d y:%d\n", i_road, x, y);
 			//コストとキャパは使わないが削除が面倒なので残している
 			add_edge(x, y, 1, 0);
 		}
@@ -110,12 +130,12 @@ int main() {
 		dfs(V, G);
 		dfs(V, revG);
 
-		int lines = m - 2*n;
-		if(DEBUG) printf("lines:%d\n", lines);
+		int lines = m - 2 * n;
+		if (DEBUG)
+			printf("lines:%d\n", lines);
 
 		//未使用の弧を削除する
-		deleteUnusedArc(V, G, false, &lines);
-		deleteUnusedArc(V, revG, true, &lines);
+		deleteUnusedArc(V, G, revG, &lines);
 
 		return 0;
 	}
